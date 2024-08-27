860219ce1b3e2f93a2c9e536d84bea01795c4d4f
hui <hui@openart.ai>
2024-08-27 19:02:09 +0800

    chore: update name

--- src/from_args.rs
+++ src/from_args.rs
@@ -1,6 +1,6 @@
 use crate::impl_from_args;
 use crate::types::ArcAny;
-use all_tuples::all_tuples;
+use emitter_all_tuples::all_tuples;
 
 pub trait FromArgs: Sized {
     fn from_args(args: &[ArcAny]) -> Option<Self>;
